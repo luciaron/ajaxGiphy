14d0732d00c477e241b46700c1f9a2fa138855c1
aaron <aaronmlucia@gmail.com>
2021-12-23 23:02:16 -0500

    link works now, correctly id buttons

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 console.log("Let's get this party started!");
 
 async function getGif (term) {
-  const url = 'api.giphy.com/v1/gifs/search';
+  const url = 'http://api.giphy.com/v1/gifs/search';
   const key = '4ov034goM9qi9KAuk1DoryhYQVCMMIe2';
-  const res = await axios.get(url, {params: { q : term, api_key: key}})
+  const res = await axios.get(url, {params: { q : term, api_key: key}});
+  console.log(res);
 };
 
 const btn = document.querySelector('#submit')
 
-btn.addEventListener(function(e){
+btn.addEventListener('click', function(e){
   e.preventDefault();
   const submission = document.querySelector('#search');
   const term = submission.value;
   getGif(term);
 })
+
