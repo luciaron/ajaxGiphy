5d187a61a12e4e7af18d225426c14a5c196d22cb
aaron <aaronmlucia@gmail.com>
2021-12-23 23:34:25 -0500

    last commit of 23 dec

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ async function getGif (term) {
   const url = 'http://api.giphy.com/v1/gifs/search';
   const key = '4ov034goM9qi9KAuk1DoryhYQVCMMIe2';
   const res = await axios.get(url, {params: { q : term, api_key: key}});
-  console.log(res);
+  const gif = res.data.data[0].url;
+  console.log(gif);
+  return gif;
 };
 
-const btn = document.querySelector('#submit')
+const btn = document.querySelector('#submit');
+const gifs = document.querySelector('#gifs');
 
 btn.addEventListener('click', function(e){
   e.preventDefault();
   const submission = document.querySelector('#search');
   const term = submission.value;
-  getGif(term);
+  placeGif(getGif(term));
 })
 
+function placeGif (src){
+  const newGif = document.createElement('img')
+  newGif.setAttribute('src', src);
+  gifs.append(newGif);
+}
