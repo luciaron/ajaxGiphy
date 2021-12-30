112add4c06828d0a7ca6d248ad59085526dc7fe3
aaron <aaronmlucia@gmail.com>
2021-12-29 19:00:10 -0500

    part 2 fixed part 3 complete

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ async function getGif (term) {
   const url = 'http://api.giphy.com/v1/gifs/search';
   const key = '4ov034goM9qi9KAuk1DoryhYQVCMMIe2';
   const res = await axios.get(url, {params: { q : term, api_key: key}});
-  const gif = res.data.data[0].url;
+  const gif = res.data.data[0].images.downsized_medium.url //note that the url works as the first url in the object image is to a page rather than the image source
   console.log(gif);
-  return gif;
+  const newGif = document.createElement('img')
+  newGif.setAttribute('src', gif);
+  gifs.prepend(newGif);
 };
 
-const btn = document.querySelector('#submit');
+const submit = document.querySelector('#submit');
+const clear = document.querySelector('#clear');
 const gifs = document.querySelector('#gifs');
 
-btn.addEventListener('click', function(e){
+submit.addEventListener('click', function(e){
   e.preventDefault();
   const submission = document.querySelector('#search');
   const term = submission.value;
-  placeGif(getGif(term));
-})
+  const src = getGif(term);
+});
 
-function placeGif (src){
-  const newGif = document.createElement('img')
-  newGif.setAttribute('src', src);
-  gifs.append(newGif);
-}
+clear.addEventListener('click', function(e){
+  e.preventDefault();
+  gifs.innerHTML = '';
+})
